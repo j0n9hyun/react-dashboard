91b05a476ebd77d1ac2b0fdd63930b266150e9c0
j0n9hyun <j0n9hyun@kakao.com>
2021-01-31 17:39:50 +0900

    fetch

--- src/components/MainBoard.js
+++ src/components/MainBoard.js
@@ -1,7 +1,29 @@
-import React from 'react';
+// // import axios from 'axios';
+// import React from 'react';
+// const MainBoard = () => {
+//   // const res = await fetch('http://localhost:8888/users');
+//   // const users = await res.json();
+//   // console.log(users);
+//   return <div>확인</div>;
+// };
+
+import React, { useEffect } from 'react';
+
+const call = async () => {
+  const res = await fetch('http://localhost:8888/users');
+  const users = await res.json();
+  if (users[0].id === 'j0n9hyun') {
+    console.log('성공');
+  } else {
+    console.log('통과실패');
+  }
+};
 
 const MainBoard = () => {
-  return <div>화긴화긴</div>;
+  useEffect(() => {
+    call();
+  });
+  return <div>확인</div>;
 };
 
 export default MainBoard;
